77f31d55a8b122b2b5d5ff52b0f2824d2bfbef26
Prathamesh <prathameshw95@gmail.com>
2024-11-24 18:41:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    print('Hello!')
+
+    arr1 = np.array([1, 2, 3, 4])
+    arr2 = np.array([5, 6, 7, 8])
+
+    arr3 = arr1 + arr2
+    print(arr3)
